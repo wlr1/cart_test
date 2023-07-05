7255c71482901c4a02617fdc0cba46b7f10a176d
wlr1 <uns4d123@gmail.com>
2023-07-05 19:35:21 +0300

    added footer, header, productList and cart components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import Header from "./components/Header.tsx";
+import ProductList from "./components/ProductList.tsx";
+import Cart from "./components/Cart.tsx";
+import Footer from "./components/Footer.tsx";
 
 const App: React.FC = () => {
-  return <div>ggf</div>;
+  return <div></div>;
 };
 
 export default App;
