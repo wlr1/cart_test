714ca94108024305a82afa6bbe1f375d7884b580
wlr1 <uns4d123@gmail.com>
2023-07-05 18:41:54 +0300

    added cart context reducer / useCartContext hook

--- src/context/ProductsProvider.tsx
+++ src/context/ProductsProvider.tsx
@@ -9,23 +9,6 @@ export type ProductType = {
 };
 
 const initState: ProductType[] = [];
-// const initState: ProductType[] = [
-//   {
-//     sku: "item001",
-//     name: "Widget",
-//     price: 9.99,
-//   },
-//   {
-//     sku: "item002",
-//     name: "Premium Widget",
-//     price: 19.99,
-//   },
-//   {
-//     sku: "item003",
-//     name: "Deluxe Widget",
-//     price: 29.99,
-//   },
-// ];
 
 export type useProductsContextType = { products: ProductType[] };
 
